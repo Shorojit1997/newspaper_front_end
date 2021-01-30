2223bce7d31b77d13ae11f5fb2808a2fb09bbf39
Shorojit1997 <shorojitsarkar1997@gmail.com>
2021-01-31 00:19:29 +0600

    first project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './Navbar/Navbar'
+import UploadNews from './pages/UploadNews';
+import RoutePage from './Routes/RoutePage';
+import HomePage from './pages/HomePage';
+
+
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+     {/* <Navbar/>
+     <UploadNews/> */}
+     <RoutePage/>
+     {/* <HomePage newstype="science"/> */}
     </div>
   );
 }
